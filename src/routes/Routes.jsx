d21c680eb59d95803f8dde9678e6cb52b10be084
rakshit-singh2 <thisisjames007@gmail.com>
2024-09-24 16:37:45 +0530

    routes correction

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,7 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "../pages/Home";
 import Products from "../pages/Products";
-import Admin from "../pages/Admin";
 import EditSettings from "../pages/EditSettings";
 
 
@@ -9,5 +8,4 @@ export const router = createBrowserRouter([
     { path: '/', element: <Home /> },
     { path: 'product/:index/:chainId', element: <Products/> },
     { path: 'settings/:index', element: <EditSettings/> },
-    { path: 'admin', element: <Admin /> },
 ]);
